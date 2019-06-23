2e76fb9214ad16f79ce14b249520ddd24deaa010
Joshua Crum <joshua.crum@gmail.com>
2019-06-23 22:33:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,20 +72,48 @@ class Dog
       new_dog
     end
 
-    def self.new_from_db(row)
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.new_from_db(row)
     id = row[0]
     name = row[1]
     breed = row[2]
     self.new(id: id, name: name, breed: breed)
   end
 
-    def update
+  def update
     sql = <<-SQL
-      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
-      SQL
+    UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+    SQL
 
-      DB[:conn].execute(sql, self.name, self.breed, self.id)
-    end
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 
 
 end
